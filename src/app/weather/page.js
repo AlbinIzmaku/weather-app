cdd5cccb40673bea0ea0182feb5160b0fd528f4e
Albin Izmaku <albinizmaku2@gmail.com>
2024-04-05 11:58:51 +0200

    adding touch events

--- src/app/weather/page.js
+++ src/app/weather/page.js
@@ -1,10 +1,51 @@
+'use client'
 import Hourly from "@/components/hourly";
-import airQuality from "/public/airQuality.svg";
 import Image from "next/image";
 import styles from "@/style/weather.module.css";
 import rectangle from "/public/rectangle.svg";
+import { useEffect, useState } from "react";
 
 export default function Weather() {
+  const hours = [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  const [time, setTime] = useState(new Date());
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(6);
+  const [startX, setStartX] = useState(0);
+  const [scrolling, setScrolling] = useState(false);
+  const midDay = time.getHours() > 11 ? "PM" : "AM";
+  const currentTime = time.getHours() % 12;
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  function handleTouchStart(e) {
+    setStartX(e.touches[0].clientX);
+    setScrolling(true);
+  }
+
+  function handleTouchMove(e) {
+    if (!scrolling) return;
+    const currentX = e.touches[0].clientX;
+    const deltaX = currentX - startX;
+    if (deltaX > 0) {
+      setA(a - 2);
+      setB(b - 2);
+    } else {
+      setA(a + 2);
+      setB(b + 2);
+    }
+    setStartX(currentX);
+  }
+
+  function handleTouchEnd() {
+    setScrolling(false);
+  }
+
   return (
     <main className={styles.main}>
       <div className={styles.ellipse3} />
@@ -20,13 +61,17 @@ export default function Weather() {
           <button>Weekly Forecast</button>
         </div>
       </div>
-      <section className={styles.hourlyContainer}>
-        <Hourly />
-        <Hourly />
-        <Hourly />
-        <Hourly />
-        <Hourly />
-        <Hourly />
+      <section
+        className={styles.hourlyContainer}
+        onTouchStart={handleTouchStart}
+        onTouchMove={handleTouchMove}
+        onTouchEnd={handleTouchEnd}
+      >
+        {hours.slice(a, b).map((hour, index) => {
+          return (
+            <Hourly key={index} time={currentTime + hour} midDay={midDay} />
+          );
+        })}
       </section>
     </main>
   );
--- src/components/hourly.jsx
+++ src/components/hourly.jsx
@@ -1,11 +1,15 @@
+"use client";
 import Image from "next/image";
 import fastWindSmallIcon from "/public/fastWindS.png";
 import styles from "@/style/hourly.module.css";
+import { useEffect, useState } from "react";
 
-export default function Hourly() {
+export default function Hourly({time, midDay}) {
+  
+
   return (
     <div className={styles.hourly}>
-      <h4>12 AM</h4>
+      <h4 suppressHydrationWarning>{time} {midDay}</h4>
       {/* image bone me svg */}
       <Image src={fastWindSmallIcon} alt="Fast Wind" width={44} height={38} />
       <h3>19°</h3>
